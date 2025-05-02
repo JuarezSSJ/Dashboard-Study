c3fcceebe397191624950d498ccbccc3ce31fca5
Juarez Santana <juarezsantanajunior@gmail.com>
2025-05-02 16:50:59 -0300

    tabela

--- app.py
+++ app.py
@@ -31,3 +31,10 @@ base_filtrada = base[
 # Exibir dados filtrados
 st.subheader("📋 Visualização da Tabela de Vendas Filtrada")
 st.dataframe(base_filtrada)
+
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
